36b9f38726e416ba308cfe4d735550e042932ddf
DanielLevi3 <66633430+DanielLevi3@users.noreply.github.com>
2020-07-22 12:21:13 +0300

    Add files via upload
    הגשתי באיחור מכיוון שלא הצלחתי לממש את המעשי בצורה טובה וחיכיתי לתרגול שהיה בשלישי שגם קצת עשה לי קצת בלאגן

---
+++ Citizen.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork17._07._20
+{
+    class Citizen
+    {
+        public string m_name;
+        public Citizen[] children = new Citizen[]
+        {
+
+        };
+        public readonly int m_fatherID;
+        public readonly int m_ID; //1:0//2:1//3:2
+        public static int m_NumberOfCurrentCitizens = 0; // 0
+        public const int maximumNumberOfCitizens = 100000;
+        public Citizen(string name, int fatherID)
+        {
+            this.m_name = name;
+            this.m_fatherID = fatherID;
+            m_NumberOfCurrentCitizens++;//0,1,2
+            m_ID = fatherID--;
+        }
+
+        public static void PrintNumberOfCitizens()
+        {
+            Console.WriteLine(m_NumberOfCurrentCitizens);
+        }
+        public static bool ReachedHalfOfMaximumSize()
+        {
+            int x = maximumNumberOfCitizens / 2;
+            if (m_NumberOfCurrentCitizens  >= x)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void PrintID()
+        {
+            Console.WriteLine(this.m_ID);
+        }
+        public void PrintGapBetweenMyIDAndFather()
+        {
+            int gap = m_fatherID - m_ID;
+            Console.WriteLine(gap);
+        }
+        public void SetChildren(Citizen[] citizen)
+        {
+            this.children = citizen;
+        }
+        public override string ToString()
+        {
+            return $"[{base.ToString()}] Name: {m_name} Children :{children} MyID : {m_ID}\n" +
+                $"MyFatherID:{m_fatherID} NumberOfCurrentCitizens : {m_NumberOfCurrentCitizens }";
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork17._07._20
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Citizen Bob = new Citizen("Robert",1324);
+            Citizen Josh = new Citizen("Josh", 0);
+            Citizen Rob = new Citizen("Rob", 0);
+            Citizen Kori = new Citizen("Kori", 0);
+            Citizen[] B = new Citizen[3] { Josh, Rob, Kori };
+            Bob.SetChildren(B);
+            Citizen.PrintNumberOfCitizens();
+            CheckValidity(Bob);
+        }
+        static bool HasChildren(Citizen citizen)
+        {
+            if (citizen.children.Count() == 0)
+                return false;
+            return true;
+        }
+        static bool CheckValidity(Citizen citizen)//10 //3: true//0:3,Fid:10//1:4,Fid:8//
+        {
+            if (citizen.children.Count() >= 1)
+            {
+                for(int i = 0; citizen.children.Length>0;i++)
+                {
+                    if (citizen.children[i].m_fatherID == citizen.m_ID)
+                        return true;
+                }
+            }    return false;
+        }
+
+    }
+}
